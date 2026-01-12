1d505ea9bb5d4b294c11a701a47e0b7c8d0d6097
tianmiao <23025006@qq.com>
2026-01-12 16:21:15 +0800

    update

---
+++ src/registry/index.ts
@@ -0,0 +1,7 @@
+export const registry = {
+  name: 'Airiot UI Library',
+  homepage: 'https://airiot.cn/ui',
+  items: [
+    
+  ],
+}
---
+++ src/registry/lib/airiot/client.ts
@@ -0,0 +1,3 @@
+import * as airiot from '@airiot/client'
+
+export default airiot
